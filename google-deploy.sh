5f114fa9f20c8c80189f9b985d2f3b16ee30f95a
Arthur Aquino <ataquino@gmail.com>
2019-01-25 16:44:43 -0200

    [CHORE] Removed teardown in gcp deployment

--- google-deploy.sh
+++ google-deploy.sh
@@ -54,4 +54,4 @@ echo "Listing pods"
 kubectl get pods
 
 echo "Teardown"
-gcloud container clusters delete $KUBERNETES_APP_NAME --quiet
+#gcloud container clusters delete $KUBERNETES_APP_NAME --quiet
